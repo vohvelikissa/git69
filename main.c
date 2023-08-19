6049645439d171ea6680694e9b15f9044545da89
Pessi <pessiv@outlook.com>
2023-08-19 22:08:43 +0300

    x

---
+++ main.c
@@ -0,0 +1,21 @@
+#include<stdlib.h>
+#include<string.h>
+
+void ft_commit (int dfi, char *committext) {
+	if (dfi == 1) {
+		system("git add .");
+	}
+	system("git add *");
+	char commit[125] = "git commit -m \"";
+	char *pcm = commit;
+	strcat(pcm, committext);
+	strcat(pcm, "\"");
+	system(commit);
+}
+int main(int count, char *args[]) {
+	int dotfilesincluded = 0;
+	if (count > 1 && args[1][0] == 'x') {
+		dotfilesincluded = 1;
+	}
+	ft_commit(dotfilesincluded, args[1]);
+}
